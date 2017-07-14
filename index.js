78a7171db110e30fa18b8edb6a3bdc8c70a1f049
Joey Cho <cjy12182000@naver.com>
2017-07-14 19:13:26 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+function getFirstSelector(selector)
+{
+  return document.querySelector(selector)
+}
+
+function nestedTarget()
+{
+  return document.getElementById("nested").querySelector('div.target')
+}
+
+function increaseRankBy(n)
+{
+  const lis = document.querySelectorAll('ul.ranked-list li')
+
+  for(let i =0; i<lis.length;i++){
+    lis[i].innerHTML= parseInt(lis[i].innerHTML)+n;
+  }
+}
+
+function deepestChild()
+{
+  var current = document.getElementById('grand-node').querySelectorAll('div');
+
+  return current[current.length-1]
+}
